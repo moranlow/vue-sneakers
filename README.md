b3dedc697daf524804eeb056954508de752d0b3f
moranlow <63882756+moranlow@users.noreply.github.com>
2024-01-29 21:58:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Vue Sneakers 👟
+
+**⚙️ Stack:**
+
+- Vue 3
+- Vue Router
+- TailwindCSS
+- Axios
+- @formkit/auto-animate
+- Composition API / Options API
+- ESLint
+- Prettier
---
+++ src/pages/Home.vue
@@ -0,0 +1,151 @@
+<script setup>
+import { reactive, watch, ref, onMounted } from 'vue'
+import axios from 'axios'
+import debounce from 'lodash.debounce'
+import { inject } from 'vue'
+import CardList from '../components/CardList.vue'
+
+const { cart, addToCart, removeFromCart } = inject('cart')
+
+const items = ref([])
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
+
+const onClickAddPlus = (item) => {
+  if (!item.isAdded) {
+    addToCart(item)
+  } else {
+    removeFromCart(item)
+  }
+}
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearchInput = debounce((event) => {
+  filters.searchQuery = event.target.value
+}, 300)
+
+const addToFavorite = async (item) => {
+  try {
+    if (!item.isFavorite) {
+      const obj = {
+        item_id: item.id
+      }
+
+      item.isFavorite = true
+
+      const { data } = await axios.post(`https://afe6aca432517c83.mokky.dev/favorites`, obj)
+
+      item.favoriteId = data.id
+    } else {
+      item.isFavorite = false
+      await axios.delete(`https://afe6aca432517c83.mokky.dev/favorites/${item.favoriteId}`)
+      item.favoriteId = null
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const fetchFavorites = async () => {
+  try {
+    const { data: favorites } = await axios.get(`https://afe6aca432517c83.mokky.dev/favorites`)
+
+    items.value = items.value.map((item) => {
+      const favorite = favorites.find((favorite) => favorite.item_id === item.id)
+
+      if (!favorite) {
+        return item
+      }
+
+      return {
+        ...item,
+        isFavorite: true,
+        favoriteId: favorite.id
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+
+    const { data } = await axios.get(`https://afe6aca432517c83.mokky.dev/items`, {
+      params
+    })
+
+    items.value = data.map((obj) => ({
+      ...obj,
+      isFavorite: false,
+      favoriteId: null,
+      isAdded: false
+    }))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+onMounted(async () => {
+  const localCart = localStorage.getItem('cart')
+  cart.value = localCart ? JSON.parse(localCart) : []
+
+  await fetchItems()
+  await fetchFavorites()
+
+  items.value = items.value.map((item) => ({
+    ...item,
+    isAdded: cart.value.some((cartItem) => cartItem.id === item.id)
+  }))
+})
+
+watch(cart, () => {
+  items.value = items.value.map((item) => ({
+    ...item,
+    isAdded: false
+  }))
+})
+
+watch(filters, fetchItems)
+</script>
+
+<template>
+  <div class="flex justify-between items-center flex-col lg:flex-row">
+    <h2 class="text-3xl font-bold mb-6 lg:mb-0">All sneakers</h2>
+
+    <div class="flex gap-4 flex-col-reverse w-full justify-between md:flex-row lg:w-auto">
+      <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
+        <option value="name">By name</option>
+        <option value="price">By price (Low to High)</option>
+        <option value="-price">By price (High to Low)</option>
+      </select>
+
+      <div class="relative">
+        <img class="absolute left-4 top-3" src="/search.svg" />
+        <input
+          @input="onChangeSearchInput"
+          class="w-full border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+          type="text"
+          placeholder="Search..."
+        />
+      </div>
+    </div>
+  </div>
+
+  <div class="mt-10">
+    <CardList :items="items" @add-to-favorite="addToFavorite" @add-to-cart="onClickAddPlus" />
+  </div>
+</template>
